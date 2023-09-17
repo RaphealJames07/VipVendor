8335f7d18c0a45f491a8864cf7207b395285692d
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-09-17 20:40:17 +0100

    working on product

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,17 @@
-require('./database/database')
-const express = require('express');
-const morgan = require('morgan');
-const userRouter = require('./routes/userRouter');
+require("./database/database");
+const express = require("express");
+const morgan = require("morgan");
+const userRouter = require("./routes/userRouter");
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4040;
 
 const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.use('/api', userRouter);
-
+app.use("/api", userRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening to PORT: ${PORT}`);
-})
+  console.log(`Server is listening to PORT: ${PORT}`);
+});
