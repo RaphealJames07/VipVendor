6e6587b3fbc686ab8ca0d16c51bf3fc341e2ae1c
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-09-22 19:47:15 +0100

    feat: product rating

--- Backend/routes/productRouter.js
+++ Backend/routes/productRouter.js
@@ -2,6 +2,10 @@ const { Router } = require("express");
 const {
   newProduct,
   getAllProducts,
+  updateProductById,
+  productRating,
+  deleteProductRating,
+  findProductById,
 } = require("../controllers/productController");
 const { authenticate, isAdmin } = require("../utils/authentication");
 
@@ -9,5 +13,18 @@ const productRouter = Router();
 
 productRouter.post("/create-product", authenticate, isAdmin, newProduct);
 productRouter.get("/find-products", getAllProducts);
+productRouter.put(
+  "/update-product/:id",
+  authenticate,
+  isAdmin,
+  updateProductById
+);
+productRouter.put("/rate-product/:productId", authenticate, productRating);
+productRouter.put(
+  "/delete-rating/:productId",
+  authenticate,
+  deleteProductRating
+);
+productRouter.get("/get-product/:id", findProductById);
 
 module.exports = productRouter;
