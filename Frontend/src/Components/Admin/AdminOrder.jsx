f40c74d29a9e66983b81f2bc4b1fd8f7a4b15fa4
RaphealJames07 <123210977+RaphealJames07@users.noreply.github.com>
2023-10-25 15:04:17 +0100

    james bluetooth

--- Frontend/src/Components/Admin/AdminOrder.jsx
+++ Frontend/src/Components/Admin/AdminOrder.jsx
@@ -53,63 +53,204 @@ const AdminOrder = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="w-full h-max bg-fuchsia-200">
-                        <div className="w-full h-16 flex ">
-                            <div className="w-16 h-full flex items-center justify-center">
+                    <div className="w-full h-max  border-y border-x border-blue-950 rounded-md bg-fuchsia-200">
+                        <div className="w-full h-16 flex gap-3 border-b border-b-blue-950 px-2">
+                            <div className="w-20  h-full flex items-center font-semibold ">
                                 ID
                             </div>
-                            <div className="w-16 h-full  flex items-center justify-center">
+                            <div className="w-24  h-full  flex items-center font-semibold ">
                                 DATE
                             </div>
-                            <div className="w-20 h-full flex items-center justify-center">NAME</div>
-                            <div className="w-20 h-full flex items-center justify-center">PRODUCT</div>
-                            <div className="w-20 h-full flex items-center justify-center">PRICE</div>
-                            <div className="w-20 h-full flex items-center justify-center">CATEGORY</div>
-                            <div className="w-20 h-full flex items-center justify-center bg-black">STATUS</div>
+                            <div className="w-48  h-full flex items-center font-semibold ">
+                                NAME
+                            </div>
+                            <div className="w-56  h-full flex items-center font-semibold ">
+                                PRODUCT
+                            </div>
+                            <div className="w-48  h-full flex items-center font-semibold ">
+                                ADDRESS
+                            </div>
+                            <div className="w-28  h-full flex items-center font-semibold ">
+                                PRICE
+                            </div>
+                            <div className="w-32  h-full flex items-center font-semibold ">
+                                CATEGORY
+                            </div>
+                            <div className="w-32  h-full flex justify-center items-center font-semibold  ">
+                                STATUS
+                            </div>
                         </div>
-                        <div className="w-full h-max">
-                            <div className="w-full h-16 flex">
-                                <div className="">00001</div>
-                                <div className="">24/10/2023</div>
-                                <div className="">John Elizabeth</div>
-                                <div className="">
+                        <div className="w-full h-[50vh] flex flex-col gap- overflow-y-auto">
+                            <div className="w-full h-16 flex gap-3 border-b border-b-blue-950 px-2 cursor-pointer">
+                                <div className="w-20  h-full flex items-center text-sm ">
+                                    00001
+                                </div>
+                                <div className="w-24  h-full flex items-center text-sm ">
+                                    24/10/2023
+                                </div>
+                                <div className="w-48  h-full flex items-center text-sm ">
+                                    John Elizabeth
+                                </div>
+                                <div className="w-56  h-full flex items-center text-sm ">
                                     Xiaomi Mi Watch Waterproof
                                 </div>
-                                <div className="">N94,000</div>
-                                <div className="">Accessories</div>
-                                <div className="">In transit</div>
+                                <div className="w-48  h-full flex items-center text-sm ">
+                                    12 Kokoyi Street Olodi Apapa Lagos
+                                </div>
+                                <div className="w-28  h-full flex items-center text-sm ">
+                                    N94,000
+                                </div>
+                                <div className="w-32  h-full flex items-center text-sm ">
+                                    Accessories
+                                </div>
+                                <div className="w-32  h-full flex justify-center items-center text-sm ">
+                                    In transit
+                                </div>
                             </div>
-                            <div className="w-full h-16 flex">
-                                <div className="">00001</div>
-                                <div className="">24/10/2023</div>
-                                <div className="">John Elizabeth</div>
-                                <div className="">
+                            <div className="w-full h-16 flex gap-3 border-b border-b-blue-950 px-2 cursor-pointer">
+                                <div className="w-20  h-full flex items-center text-sm ">
+                                    00001
+                                </div>
+                                <div className="w-24  h-full flex items-center text-sm ">
+                                    24/10/2023
+                                </div>
+                                <div className="w-48  h-full flex items-center text-sm ">
+                                    John Elizabeth
+                                </div>
+                                <div className="w-56  h-full flex items-center text-sm ">
                                     Xiaomi Mi Watch Waterproof
                                 </div>
-                                <div className="">N94,000</div>
-                                <div className="">Accessories</div>
-                                <div className="">In transit</div>
+                                <div className="w-48  h-full flex items-center text-sm ">
+                                    12 Kokoyi Street Olodi Apapa Lagos
+                                </div>
+                                <div className="w-28  h-full flex items-center text-sm ">
+                                    N94,000
+                                </div>
+                                <div className="w-32  h-full flex items-center text-sm ">
+                                    Accessories
+                                </div>
+                                <div className="w-32  h-full flex justify-center items-center text-sm ">
+                                    In transit
+                                </div>
                             </div>
-                            <div className="w-full h-16 flex">
-                                <div className="">00001</div>
-                                <div className="">24/10/2023</div>
-                                <div className="">John Elizabeth</div>
-                                <div className="">
+                            <div className="w-full h-16 flex gap-3 border-b border-b-blue-950 px-2 cursor-pointer">
+                                <div className="w-20  h-full flex items-center text-sm ">
+                                    00001
+                                </div>
+                                <div className="w-24  h-full flex items-center text-sm ">
+                                    24/10/2023
+                                </div>
+                                <div className="w-48  h-full flex items-center text-sm ">
+                                    John Elizabeth
+                                </div>
+                                <div className="w-56  h-full flex items-center text-sm ">
                                     Xiaomi Mi Watch Waterproof
                                 </div>
-                                <div className="">N94,000</div>
-                                <div className="">Accessories</div>
-                                <div className="">In transit</div>
+                                <div className="w-48  h-full flex items-center text-sm ">
+                                    12 Kokoyi Street Olodi Apapa Lagos
+                                </div>
+                                <div className="w-28  h-full flex items-center text-sm ">
+                                    N94,000
+                                </div>
+                                <div className="w-32  h-full flex items-center text-sm ">
+                                    Accessories
+                                </div>
+                                <div className="w-32  h-full flex justify-center items-center text-sm ">
+                                    In transit
+                                </div>
                             </div>
+                            <div className="w-full h-16 flex gap-3 border-b border-b-blue-950 px-2 cursor-pointer">
+                                <div className="w-20  h-full flex items-center text-sm ">
+                                    00001
+                                </div>
+                                <div className="w-24  h-full flex items-center text-sm ">
+                                    24/10/2023
+                                </div>
+                                <div className="w-48  h-full flex items-center text-sm ">
+                                    John Elizabeth
+                                </div>
+                                <div className="w-56  h-full flex items-center text-sm ">
+                                    Xiaomi Mi Watch Waterproof
+                                </div>
+                                <div className="w-48  h-full flex items-center text-sm ">
+                                    12 Kokoyi Street Olodi Apapa Lagos
+                                </div>
+                                <div className="w-28  h-full flex items-center text-sm ">
+                                    N94,000
+                                </div>
+                                <div className="w-32  h-full flex items-center text-sm ">
+                                    Accessories
+                                </div>
+                                <div className="w-32  h-full flex justify-center items-center text-sm ">
+                                    In transit
+                                </div>
+                            </div>
+                            <div className="w-full h-16 flex gap-3 border-b border-b-blue-950 px-2 cursor-pointer">
+                                <div className="w-20  h-full flex items-center text-sm ">
+                                    00001
+                                </div>
+                                <div className="w-24  h-full flex items-center text-sm ">
+                                    24/10/2023
+                                </div>
+                                <div className="w-48  h-full flex items-center text-sm ">
+                                    John Elizabeth
+                                </div>
+                                <div className="w-56  h-full flex items-center text-sm ">
+                                    Xiaomi Mi Watch Waterproof
+                                </div>
+                                <div className="w-48  h-full flex items-center text-sm ">
+                                    12 Kokoyi Street Olodi Apapa Lagos
+                                </div>
+                                <div className="w-28  h-full flex items-center text-sm ">
+                                    N94,000
+                                </div>
+                                <div className="w-32  h-full flex items-center text-sm ">
+                                    Accessories
+                                </div>
+                                <div className="w-32  h-full flex justify-center items-center text-sm ">
+                                    In transit
+                                </div>
+                            </div>
+                            <div className="w-full h-16 flex gap-3 border-b border-b-blue-950 px-2 cursor-pointer">
+                                <div className="w-20  h-full flex items-center text-sm ">
+                                    00001
+                                </div>
+                                <div className="w-24  h-full flex items-center text-sm ">
+                                    24/10/2023
+                                </div>
+                                <div className="w-48  h-full flex items-center text-sm ">
+                                    John Elizabeth
+                                </div>
+                                <div className="w-56  h-full flex items-center text-sm ">
+                                    Xiaomi Mi Watch Waterproof
+                                </div>
+                                <div className="w-48  h-full flex items-center text-sm ">
+                                    12 Kokoyi Street Olodi Apapa Lagos
+                                </div>
+                                <div className="w-28  h-full flex items-center text-sm ">
+                                    N94,000
+                                </div>
+                                <div className="w-32  h-full flex items-center text-sm ">
+                                    Accessories
+                                </div>
+                                <div className="w-32  h-full flex justify-center items-center text-sm ">
+                                    In transit
+                                </div>
+                            </div>
+                            
+                            
+                            
+                            
+                            
                         </div>
                     </div>
-                    <div className="w-full h-20 bg-lime-100">
-                        <div className="">
+                    <div className="w-full h-20 bg-lime-100 flex items-center justify-between">
+                        <div className="w-80 pl-10">
                             <p>
                                 Showing <span>1-13 results of 150 orders</span>
                             </p>
                         </div>
-                        <div className="">
+                        <div className="w-80 ">
                             <Pagination defaultCurrent={1} total={50} />
                         </div>
                     </div>
